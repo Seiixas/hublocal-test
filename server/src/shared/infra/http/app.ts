0cb8e93b9e93f0b9fc962ee72ccf7bf329d8c555
Mateus Seixas <mateuseixas@icloud.com>
2022-05-18 14:27:46 -0300

    feat: general - api docs with swagger

--- server/src/shared/infra/http/app.ts
+++ server/src/shared/infra/http/app.ts
@@ -2,7 +2,9 @@ import 'reflect-metadata';
 import '../../container';
 
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 
+import swaggerFile from '../../../swagger.json';
 import { handleErrors } from './middlewares/handleErrors';
 import { routes } from './routes';
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1', routes);
+app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(handleErrors);
 
 export { app };
