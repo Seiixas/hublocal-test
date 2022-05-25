5984c47051042e7366e95fcc55501ce7d9acb036
Mateus Seixas <mateuseixas@icloud.com>
2022-05-25 08:45:14 -0300

    .

---
+++ web/src/components/companies/View/index.tsx
@@ -0,0 +1,3 @@
+export function View() {
+  return <h1>Hello World</h1>
+}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Create } from "../components/companies/Create";
+import { View } from "../components/companies/View";
 import { Singin } from "../components/Signin";
 import { Signup } from "../components/Signup";
 
@@ -8,6 +9,7 @@ export function Paths() {
     <BrowserRouter>
       <Routes>
         <Route path="/companies/create" element={<Create />}/>
+        <Route path="/companies/view" element={<View />}/>
         <Route path="/signin" element={<Singin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
